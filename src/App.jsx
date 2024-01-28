1705a8edb3bad1d53a100c499fcc16f995f87ae5
naimuddin94 <developernaimuddin@gmail.com>
2024-01-29 01:39:47 +0600

    input field create

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-
-import './App.css'
+import { useSelector } from "react-redux";
 
 function App() {
-  
+  const { todos, status } = useSelector((state) => state.todos);
 
   return (
-    <>
-      <h1>hello world</h1>
-    </>
-  )
+    <div className="container">
+      <div>
+        <form>
+          <input type="text" />
+          <button>Add Todo</button>
+        </form>
+      </div>
+      <div>
+        <h2>Redux Toolkit Practice</h2>
+        <div>
+          {todos &&
+            todos.map((todo) => (
+              <article key={todo.id} className="article">
+                <h1>{todo.id}</h1>
+                <h3>{todo.text}</h3>
+                <button>Update</button>
+                <button>Delete</button>
+              </article>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/store/store.js
+++ src/store/store.js
@@ -3,7 +3,7 @@ import todoReducer from "../features/todoSlice";
 
 const store = configureStore({
   reducer: {
-    todo: todoReducer,
+    todos: todoReducer,
   },
 });
 
